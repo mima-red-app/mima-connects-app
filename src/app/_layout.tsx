88ada955e9bd6d5f9e5603a7aa001a80702cedff
Estarlin German <estarlingerman17@gmail.com>
2026-09-08 12:49:01 -0400

    Mejoras visuales en la pantalla de home , creacion de nuevos componentes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,16 +2,53 @@ import { Stack } from "expo-router";
 import { HeroUINativeProvider } from "heroui-native/provider";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+  Poppins_800ExtraBold,
+} from "@expo-google-fonts/poppins";
+import { useAuth } from "@/features/auth/hooks/useAuth";
+import Loader from "@/components/Loader";
 import "../global.css";
 
 export default function RootLayout() {
-  return (
+  const { user, isLoading } = useAuth();
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return <Loader />;
+  }
+
+  const providers = (children: React.ReactNode) => (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
-        <HeroUINativeProvider>
-          <Stack />
-        </HeroUINativeProvider>
+        <HeroUINativeProvider>{children}</HeroUINativeProvider>
       </SafeAreaProvider>
     </GestureHandlerRootView>
+  );
+
+  if (isLoading) {
+    return providers(<Loader />);
+  }
+
+  return providers(
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Protected guard={!user}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="(auth)" />
+      </Stack.Protected>
+      <Stack.Protected guard={!!user}>
+        <Stack.Screen name="(tab)" />
+      </Stack.Protected>
+    </Stack>
   );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,10 @@
-import { Button } from "heroui-native/button";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Redirect } from "expo-router";
+import { useAuth } from "@/features/auth/hooks/useAuth";
 
 export default function Index() {
-  return (
-    <SafeAreaView
-      edges={["top", "bottom"]}
-      className="flex-1 items-center justify-center bg-background"
-    >
-      <Button className="mb-6 mx-5" onPress={() => console.log("Pressed!")}>
-        Get Started
-      </Button>
-    </SafeAreaView>
-  );
+  const { user, isLoading } = useAuth();
+
+  if (isLoading) return null;
+
+  return <Redirect href={user ? "/(tab)/home" : "/login"} />;
 }
